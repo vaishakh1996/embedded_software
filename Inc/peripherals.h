95541366908edf337d4bfa0c3884594a85dc6a4f
Pvaishakh <pvaishakh96@gmail.com>
2025-11-18 22:45:18 +0100

    Define base addresses and register pointers for RCC APB1ENR and TIM2 prescaler, ARR, CR1 registers

--- Inc/peripherals.h
+++ Inc/peripherals.h
@@ -17,11 +17,18 @@
 #define RCC_CR         (*(volatile uint32_t *)(RCC_BASE + 0x00))
 #define RCC_CFGR       (*(volatile uint32_t *)(RCC_BASE + 0x04))
 #define RCC_APB2ENR    (*(volatile uint32_t *)(RCC_BASE + 0x18))
+#define RCC_APB1ENR (*((volatile uint32_t *)(RCC_BASE + 0x1C)))
 
 #define FLASH_ACR      (*(volatile uint32_t *)(FLASH_BASE + 0x00))
 
 #define GPIOA_CRL      (*(volatile uint32_t *)(GPIOA_BASE + 0x00))
 #define GPIOA_BSRR     (*(volatile uint32_t *)(GPIOA_BASE + 0x10))
+
+
+#define TIM2_BASE 0x40000000U
+#define TIM2_PSC (*((volatile uint32_t *)(TIM2_BASE + 0x28)))
+#define TIM2_ARR (*((volatile uint32_t *)(TIM2_BASE + 0x2C)))
+#define TIM2_CR1 (*((volatile uint32_t *)(TIM2_BASE + 0x00)))
 
 
 #endif /* PERIPHERALS_H_ */
