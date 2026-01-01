291bb8f392e158b94cbb7bbd7d6b0e9bbcfb9401
Pvaishakh <pvaishakh96@gmail.com>
2026-01-01 17:16:08 +0100

    feat: add system clock init with HSE->PLLx4 (32MHz) and MCO on PA8

---
+++ Inc/system_clock.h
@@ -0,0 +1,14 @@
+/*
+ * system_clock.h
+ *
+ *  Created on: Jan 1, 2026
+ *      Author: vaishakh
+ */
+
+// system_clock.h
+#ifndef SYSTEM_CLOCK_H
+#define SYSTEM_CLOCK_H
+
+void system_clk_init(void);
+
+#endif
